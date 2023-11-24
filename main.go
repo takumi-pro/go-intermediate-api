4abf795f88de8b124f1be1667de3a18dbcbea393
takumi <takumidiary.0927@gmail.com>
2023-11-24 21:17:59 +0900

    insert data

--- main.go
+++ main.go
@@ -25,32 +25,62 @@ func main() {
 	dbDatabase := "go-api-db"
 	dbConn := fmt.Sprintf("host=localhost port=5434 user=%s "+
 		"password=%s dbname=%s sslmode=disable", dbUser, dbPassword, dbDatabase)
+
+	// データ取得処理
+	// db, err := sql.Open("postgres", dbConn)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// defer db.Close()
+
+	// articleID := 100
+	// const sqlStr = `select * from articles where article_id = $1`
+
+	// row := db.QueryRow(sqlStr, articleID)
+	// if err := row.Err(); err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// var article models.Article
+	// var createdTime sql.NullTime
+	// err = row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// if createdTime.Valid {
+	// 	article.CreatedAt = createdTime.Time
+	// }
+
+	// fmt.Printf("%+v\n", article)
+
+	// データ挿入処理
 	db, err := sql.Open("postgres", dbConn)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer db.Close()
 
-	articleID := 100
-	const sqlStr = `select * from articles where article_id = $1`
-
-	row := db.QueryRow(sqlStr, articleID)
-	if err := row.Err(); err != nil {
-		fmt.Println(err)
-		return
+	article := models.Article{
+		Title:    "insert data",
+		Contents: "this is insert data",
+		UserName: "takumi",
 	}
+	const sqlStr = `
+		insert into articles (title, contents, username, nice, created_at) values
+		($1,$2,$3,0,now())
+		returning article_id;
+	`
 
-	var article models.Article
-	var createdTime sql.NullTime
-	err = row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
-	if createdTime.Valid {
-		article.CreatedAt = createdTime.Time
-	}
-
-	fmt.Printf("%+v\n", article)
+	var newArticleID int
+	err = db.QueryRow(sqlStr, article.Title, article.Contents, article.UserName).Scan(&newArticleID)
+	fmt.Println(result.LastInsertId())
+	fmt.Println(result.RowsAffected())
 }
